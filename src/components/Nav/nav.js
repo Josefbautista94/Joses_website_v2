69184906b36d01d17684c8de26ede4ea60026d69
Jose Bautista <Josefbautista94@gmail.com>
2023-02-14 04:29:29 -0500

    added a clock and calander to my navbar

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -45,6 +45,7 @@ class Nav extends Component {
               Jose F. Bautista
             </a>
           </li>
+          <Clock className="clock" />
           <li className="codeWars">
             <img
               src="https://www.codewars.com/users/Josefbautista94/badges/large"
@@ -54,6 +55,33 @@ class Nav extends Component {
         </ul>
       </div>
     );
+  }
+}
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleString(),
+    };
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  tick() {
+    this.setState({
+      time: new Date().toLocaleString(),
+    });
+  }
+
+  render() {
+    return <li className="clock">{this.state.time}</li>;
   }
 }
 
