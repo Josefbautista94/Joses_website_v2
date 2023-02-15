7d67bda77a6662f7331eb892443847174bd9bcf7
Jose Bautista <Josefbautista94@gmail.com>
2023-02-15 18:09:59 -0500

    Added a form to my Contact page so People can drop their phone number email and name still need to touch up on it a bit

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,12 +4,80 @@ import "./Contact.css";
 class Contact extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+    };
   }
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { name, email, phone, message } = this.state;
+    console.log(
+      `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`
+    );
+  };
+
   render() {
     return (
       <div>
-        <header className="Contact-content"></header>
+        <header className="Contact-content">
+          <body className="contactPage">
+            <h1>Contact Me Here At:</h1>
+            <h3>Fill out the form below to get in touch:</h3>
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="name">Name:</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  value={this.state.name}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email:</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={this.state.email}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="phone">Phone:</label>
+                <input
+                  type="tel"
+                  name="phone"
+                  id="phone"
+                  value={this.state.phone}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">Message:</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  value={this.state.message}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <button type="submit">Submit</button>
+            </form>
+          </body>
+        </header>
       </div>
     );
   }
