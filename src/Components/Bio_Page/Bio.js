83603f7124c22fead82649d46260f2869837b818
Jose Bautista <Josefbautista94@gmail.com>
2023-02-15 02:20:47 -0500

    Got everything routed in my navbar to lead to diffrent parts of my site or linkedIn orrr Github.

---
+++ src/Components/Bio_Page/Bio.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./Bio.css";
+import { Link } from "react-router-dom";
+
+class Bio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <header className="Bio-content"></header>
+      </div>
+    );
+  }
+}
+export default Bio;
---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./Contact.css";
+
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <header className="Contact-content"></header>
+      </div>
+    );
+  }
+}
+
+export default Contact;
