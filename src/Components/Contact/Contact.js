ef361c7dce5125da9ab0eff7c5b15f6436c2c561
Jose Bautista <Josefbautista94@gmail.com>
2023-02-15 19:56:36 -0500

    changed my contact from form to what i used to have for now because there was a webpack issue

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,82 +1,19 @@
 import React, { Component } from "react";
 import "./Contact.css";
+// import logo from '../Images/58485698e0bb315b0f7675a8.png'
 
 class Contact extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
-      phone: "",
-      message: "",
-    };
-  }
-
-  handleInputChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const { name, email, phone, message } = this.state;
-    console.log(
-      `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`
-    );
-  };
-
   render() {
     return (
       <div>
-        <header className="Contact-content">
-          <body className="contactPage">
-            <h1>Contact Me Here At:</h1>
-            <h3>Fill out the form below to get in touch:</h3>
-            <form onSubmit={this.handleSubmit}>
-              <div className="form-group">
-                <label htmlFor="name">Name:</label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  value={this.state.name}
-                  onChange={this.handleInputChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">Email:</label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  value={this.state.email}
-                  onChange={this.handleInputChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="phone">Phone:</label>
-                <input
-                  type="tel"
-                  name="phone"
-                  id="phone"
-                  value={this.state.phone}
-                  onChange={this.handleInputChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="message">Message:</label>
-                <textarea
-                  name="message"
-                  id="message"
-                  value={this.state.message}
-                  onChange={this.handleInputChange}
-                />
-              </div>
-              <button type="submit">Submit</button>
-            </form>
-          </body>
+        <header className="contactPage">
+          <h1>Contact Me Here At:</h1>
+          <h3>Click The Mail Icon To Shoot Me An E-Mail!</h3>
+          {/* <img src={logo} className="Email-pic" alt="logo" /> */}
+
+          <a class="mailto" href="mailto:JoseF.Bautista22@gmail.com"></a>
+
+          <h1>JoseF.Bautista22@gmail.com</h1>
         </header>
       </div>
     );
