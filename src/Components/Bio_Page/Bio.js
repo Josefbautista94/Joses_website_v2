d7fcec587fa25c02694324c56b4b0fa43a5e685e
Jose <josefbautista94@gmail.com>
2023-09-26 22:21:33 -0400

    Added another pargaraph to my bio

--- src/Components/Bio_Page/Bio.js
+++ src/Components/Bio_Page/Bio.js
@@ -26,7 +26,7 @@ class Bio extends Component {
                     alt="A Picture of me at a hike at Sams Point NY"
                   />
 
-            <div className="paragraphContainer" id="section1">
+<div className="paragraphContainer" id="section1">
     <p className="bioParagraph">
         Hello everyone! My name is Jose Francisco Bautista. I'm a proud Mexican American from New York City, born in Harlem and raised in Washington Heights. As a Computer Science graduate from Lehman College, I'm always on the lookout for opportunities to hone my programming skills and craft innovative web applications.
     </p>
@@ -34,12 +34,16 @@ class Bio extends Component {
         My passion for technology began in childhood, and I've been enamored with it ever since. I thrive on challenges, constantly pushing my boundaries to grow as a developer. Outside the realm of code, I'm deeply intrigued by the intricacies of the human mind. I often delve into psychology and philosophy books, seeking insights into human behavior and thought processes.
     </p>
     <p className="bioParagraph">
-        When I'm not engrossed in books or code, you might find me exploring new travel destinations, staying active with gym sessions or city jogs, or indulging in my love for anime and true crime documentaries. I cherish the thrill of tackling challenging coding problems and relish opportunities to connect with new people.
+        Beyond the digital world, I have a flair for mixology, holding a license from the prestigious American Bartenders School. This allows me to blend creativity with precision, much like coding, but in the vibrant world of cocktails. It's always a pleasure to craft a drink that brings a smile to someone's face.
+    </p>
+    <p className="bioParagraph">
+        When I'm not engrossed in books, code, or experimenting with new cocktail recipes, you might find me exploring new travel destinations, staying active with gym sessions or city jogs, or indulging in my love for anime and true crime documentaries. I cherish the thrill of tackling challenging coding problems and relish opportunities to connect with new people.
     </p>
     <p className="bioParagraph">
         Driven by a desire to make a positive mark in the tech world and beyond, I'm always eager to collaborate with individuals who share this vision.
     </p>
 </div>
+
           </div>
           <div className="bottomOfPage">
             <a className="resumeButton" href={Pdf}>
